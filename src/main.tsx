22ef7f8850916e96e369256f83be73b712305523
Yannick-Deraedt <yannick.deraedt@hotmail.com>
2025-10-23 16:42:32 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+// LET OP:
+// GEEN Confetti import of gebruik hier! Enkel App renderen.
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
